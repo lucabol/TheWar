b58177559e19ad187e5c66ec4f1d233661e7a369
Luca Bolognese <lucabol@microsoft.com>
2021-09-28 11:33:41 +0200

    Small changes

--- perf.bat
+++ perf.bat
@@ -3,10 +3,10 @@ copy zigpacked\zig-out\bin\zigpacked.exe tmp\ > nul 2>&1
 copy zig\zig-out\bin\zig.exe tmp\ > nul 2>&1
 copy zigcomp\zig-out\bin\zigcomp.exe tmp\ > nul 2>&1
 copy rust\target\release\rust.exe tmp\ > nul 2>&1
-copy csharp\bin\Release\net6.0\win-x64\publish\csharp.exe tmp\ > nul 2>&1
-hyperfine --warmup 10 --min-runs 100 tmp\zigpacked.exe tmp\zigcomp.exe tmp\zig.exe tmp\rust.exe tmp\csharp.exe csharp\bin\Release\net6.0\win-x64\csharp.exe --export-markdown result.mkd
+copy csharp\bin\Release\net6.0\win-x64\publish\csharp.exe tmp\csharpAot.exe > nul 2>&1
+hyperfine --warmup 10 tmp\zigpacked.exe tmp\zigcomp.exe tmp\zig.exe tmp\rust.exe tmp\csharpAot.exe csharp\bin\release\net6.0\csharp.exe --export-markdown result.mkd
 echo.
 echo ----------------------------------------------------------------------
 echo.
-cmd /c "dir tmp /o:s /a:a" | grep /
+cmd /c "dir tmp /o:s /a:a" | grep exe
 glow -s light .\result.mkd
--- results.bat
+++ results.bat
@@ -1,1 +1,4 @@
+@echo off
+@echo .
+cmd /c "dir tmp /o:s /a:a" | grep exe
 glow -w 150 -s light .\stable_results.mkd
